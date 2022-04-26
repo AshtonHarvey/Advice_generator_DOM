4f79a4e58647668dbd46b85feb34f1df8759f209
Ashton Harvey <Ashtonh36@gmail.com>
2022-04-26 19:01:20 -0400

    Setting the stage for dynamic card creation

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,23 @@
 import "../scss/index.scss";
 
-document.getElementById("results").innerHTML = `
-<div>
-<div className="card">
-    <h2></h2>
-    <p></p>
-</div>
-This is the area that the cards of advice will go in.
-</div>
+document.getElementById("cards").innerHTML = `
+${createCard()};
+${createCard()};
+${createCard()};
+${createCard()};
+${createCard()};
+${createCard()};
+${createCard()};
+${createCard()};
+
 `;
+// flexwrap and flex grow for wide display to use specific number of colunms
+function createCard(){
+    return `<div class="card">
+    <p class="card__title">Advice #3</p>
+    <p class="card__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est iusto quis explicabo excepturi quae temporibus odit minus officia provident odio?</p>
+    </div>`
+}
 //gradient randomizer for the cards rather than images
 //emmet only works with tab or ctr space in js
+
